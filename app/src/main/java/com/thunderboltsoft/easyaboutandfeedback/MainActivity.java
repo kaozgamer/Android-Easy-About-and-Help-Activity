136cbabf162a0f193983f4eb4850741269a2b939
kaozgamer <ktdp11195@yahoo.com>
2017-02-26 21:23:55 +0800

    Initial commit

---
+++ app/src/main/java/com/thunderboltsoft/easyaboutandfeedback/MainActivity.java
@@ -0,0 +1,36 @@
+package com.thunderboltsoft.easyaboutandfeedback;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_help: // User clicks on "About & Feedback" button on toolbar
+                Intent intent = new Intent();
+                intent.setClassName(this, "com.kaozgamer.easyaboutandfeedback.AboutActivity");
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ easyaboutandfeedback/src/androidTest/java/com/kaozgamer/easyaboutandfeedback/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.kaozgamer.easyaboutandfeedback;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(JUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.kaozgamer.easyaboutandfeedback.test", appContext.getPackageName());
+    }
+}
