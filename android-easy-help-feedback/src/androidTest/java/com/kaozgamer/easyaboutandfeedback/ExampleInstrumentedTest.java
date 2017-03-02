5c711477da51a8f767831f277279128d5aff60f6
kaozgamer <ktdp11195@yahoo.com>
2017-03-02 18:58:35 +0800

    Reformat code and optimize imports.

--- android-easy-help-feedback/src/androidTest/java/com/kaozgamer/easyaboutandfeedback/ExampleInstrumentedTest.java
+++ android-easy-help-feedback/src/androidTest/java/com/kaozgamer/easyaboutandfeedback/ExampleInstrumentedTest.java
@@ -2,7 +2,6 @@ package com.kaozgamer.easyaboutandfeedback;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
